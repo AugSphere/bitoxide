68d9d1f870c7ca327068317b1fdb7fcc7949582f
AugSphere <augsphere@gmail.com>
2023-12-18 23:10:41 +0100

    Add crate and module docs

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,10 +1,10 @@
 use bitburner_api::parse_args;
 use bitburner_api::wasm_bindgen;
-use bitburner_api::Args;
+use bitburner_api::Arg;
 
 #[wasm_bindgen]
 pub fn main_rs(ns: &bitburner_api::NS) {
-    let args: Vec<Args> = parse_args(ns.args()).unwrap();
+    let args: Vec<Arg> = parse_args(ns.args()).unwrap();
 
     let mut buffer = String::new();
     buffer += &format!("Hello, world! I said {:?}", args);
