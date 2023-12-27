ed679ad94af17fabfb6b436cae74144d96c0a9c0
AugSphere <augsphere@gmail.com>
2023-12-27 13:20:58 +0100

    Update defs to 2.5.2

--- bitburner_api/src/lib.rs
+++ bitburner_api/src/lib.rs
@@ -2,7 +2,7 @@
 //!
 //! Thank you, [github.com/paulcdejean](https://github.com/paulcdejean)
 //!
-//! Created from the definitions of Bitburner v2.5.1.
+//! Created from the definitions of Bitburner v2.5.2.
 //!
 //! Note that some functions have different signatures from their Bitburner
 //! counterparts. For example [`NS::sleep`] does not have a return value (in
