38bef86b8a5c3d8cf45df2fd688e774e902f577a
AugSphere <augsphere@gmail.com>
2024-01-29 18:36:30 +0100

    Switch to a RefCell deque for channel

--- script_lib/src/simple_channel.rs
+++ script_lib/src/simple_channel.rs
@@ -1,35 +1,51 @@
-use std::sync::mpsc;
-
 mod error;
+use std::cell::RefCell;
+use std::collections::VecDeque;
+use std::rc::Rc;
+
 use error::*;
 
 #[derive(Debug, Clone)]
 pub struct Sender<T> {
-    tx: mpsc::Sender<T>,
+    tx: Rc<RefCell<VecDeque<T>>>,
 }
 
 #[derive(Debug)]
 pub struct Receiver<T> {
-    rx: mpsc::Receiver<T>,
+    rx: Rc<RefCell<VecDeque<T>>>,
 }
 
 pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
-    let (tx, rx) = mpsc::channel::<T>();
+    let deque = VecDeque::new();
+    let tx = Rc::new(RefCell::new(deque));
+    let rx = tx.clone();
     (Sender { tx }, Receiver { rx })
 }
 
 impl<T> Sender<T> {
     pub fn send(&self, t: T) -> Result<(), SendError<T>> {
-        self.tx.send(t).map_err(|x| x.into())
+        match self.tx.try_borrow_mut() {
+            Ok(mut tx) => {
+                tx.push_back(t);
+                Ok(())
+            }
+            Err(_) => Err(SendError(t)),
+        }
     }
 }
 impl<T> Receiver<T> {
     pub fn recv(&self) -> Result<T, RecvError> {
-        self.rx.recv().map_err(|_| RecvError)
+        match self.rx.try_borrow_mut() {
+            Ok(mut rx) => match rx.pop_front() {
+                Some(v) => Ok(v),
+                None => Err(RecvError::Empty),
+            },
+            Err(_) => Err(RecvError::FailedToBorrow),
+        }
     }
 
     pub fn try_recv(&self) -> Result<T, RecvError> {
-        self.rx.try_recv().map_err(|_| RecvError)
+        self.recv()
     }
 
     pub fn try_iter(&self) -> TryIter<'_, T> {
--- script_lib/src/simple_channel/error.rs
+++ script_lib/src/simple_channel/error.rs
@@ -1,8 +1,7 @@
-use std::sync::mpsc;
 use std::{error, fmt};
 
 #[derive(Clone, Copy, PartialEq, Eq)]
-pub struct SendError<T>(T);
+pub struct SendError<T>(pub T);
 
 impl<T> fmt::Debug for SendError<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -12,24 +11,24 @@ impl<T> fmt::Debug for SendError<T> {
 
 impl<T> fmt::Display for SendError<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        "failed to send through the channel".fmt(f)
+        "failed to borrow for sending".fmt(f)
     }
 }
 
 impl<T> error::Error for SendError<T> {}
 
-impl<T> From<mpsc::SendError<T>> for SendError<T> {
-    fn from(value: mpsc::SendError<T>) -> Self {
-        SendError(value.0)
-    }
-}
-
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub struct RecvError;
+pub enum RecvError {
+    Empty,
+    FailedToBorrow,
+}
 
 impl fmt::Display for RecvError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        "failed to receive".fmt(f)
+        match *self {
+            RecvError::Empty => "receiving from an empty queue".fmt(f),
+            RecvError::FailedToBorrow => "failed to borrow the queue".fmt(f),
+        }
     }
 }
 
